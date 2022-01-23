2e593cfa9bfbc71f92d37ab24f81248f4cd5dbd2
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2022-01-22 16:36:19 -0800

    updated files

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
     apiKey: "AIzaSyCUIzaDMYxC823OA1A247sOUzNlVJ4goLM",
     authDomain: "starhacks-ii.firebaseapp.com",
     projectId: "starhacks-ii",
@@ -15,9 +9,18 @@
     messagingSenderId: "343896327372",
     appId: "1:343896327372:web:3570df5c6c034f442d6e69",
     measurementId: "G-Y6PHDH220F"
-  };
+};
+
+const app = initializeApp(firebaseConfig); 
+ 
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
+
